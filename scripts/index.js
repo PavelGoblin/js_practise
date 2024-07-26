88a43f32673c763b452f8cdf2aa2890bccbe2c64
PavelGoblin <pvlbrhm@gmail.com>
2024-07-27 00:35:33 +0600

    first commit for this repository

---
+++ scripts/index.js
@@ -0,0 +1,11 @@
+/* console.log('I am coming from index.js')
+ This method is used to clear the console
+ 
+ 
+
+b
+git branch -M main
+git remote add origin https://github.com/PavelGoblin/js_practise.git
+git push -u origin main
+ */
+console.clear()
